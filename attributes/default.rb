fbce0fb8d8aa037d19509d8aefda749331a70c6a
Thomas Polliard <thomas.polliard@jmfamily.com>
2017-03-22 14:01:57 -0400

    Hard set attributes just to get this working the first time in a arch cookbook thanks to Johnb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,23 +1,30 @@
 # Terraform install package
 default['chef_workflow_terraform']['install']           = true
-default['chef_workflow_terraform']['install_url']       = 'https://releases.hashicorp.com/terraform/0.7.5/terraform_0.7.5_linux_amd64.zip'
-default['chef_workflow_terraform']['package_checksum']  = '7def82015b3a9a1bab13b4548e4c8d4ac960322a815cff7d9ebf76ef74a4cb34'
+default['chef_workflow_terraform']['install_url']       = 'https://releases.hashicorp.com/terraform/0.9.1/terraform_0.9.1_linux_amd64.zip'
+default['chef_workflow_terraform']['package_checksum']  = 'b3b18a719258dcc02b7b972eedf417be0b497e4129063711bca82877dbe65553'
 
 # S3 Bucket to store Terraform remote state files
-default['chef_workflow_terraform']['s3_bucket_name']    = ""
-default['chef_workflow_terraform']['s3_bucket_region']  = ""
+default['chef_workflow_terraform']['s3_bucket_name']    = "jmfe-terraform-backend-us-east-1"
+default['chef_workflow_terraform']['s3_bucket_region']  = "us-east-1"
 
 # EC2 Instance Configuration
-default['chef_workflow_terraform']['aws_resource_prefix']   = ""
-default['chef_workflow_terraform']['aws_region']            = ""
-default['chef_workflow_terraform']['ec2_instance_type']     = ""
-default['chef_workflow_terraform']['ec2_ami_id']            = ""
-default['chef_workflow_terraform']['ec2_login_user']        = ""
-default['chef_workflow_terraform']['ec2_keypair_name']      = ""
-default['chef_workflow_terraform']['ec2_connection_type']   = "" 
-default['chef_workflow_terraform']['vpc_security_groups']   = [""]
-default['chef_workflow_terraform']['vpc_subnet']            = ""
+default['chef_workflow_terraform']['aws_resource_prefix']   = "automate"
+default['chef_workflow_terraform']['aws_region']            = "us-east-1"
+default['chef_workflow_terraform']['ec2_instance_type']     = "t2.small"
+# default['chef_workflow_terraform']['ec2_ami_id']            = ""
+default['chef_workflow_terraform']['ec2_login_user']        = "ec2-user"
+default['chef_workflow_terraform']['ec2_keypair_name']      = "jmfe_chefkitchen"
+default['chef_workflow_terraform']['ec2_connection_type']   = "ssh" #rdp|winrm
+default['chef_workflow_terraform']['vpc_security_groups']   = ["sg-2933ee54"]
+default['chef_workflow_terraform']['vpc_subnet']            = "subnet-e984eac2"
 
 # Chef Client bootstrap configuration
-default['chef_workflow_terraform']['chef_server_url']     = ""
-default['chef_workflow_terraform']['chef_user_name']      = ""
+default['chef_workflow_terraform']['chef_server_url']     = "https://chef.jmfamily.com/organizations/jm_family"
+# default['chef_workflow_terraform']['chef_user_name']      = ""
+default['chef_workflow_terraform']['chef_validator_pem_name'] = 'jm_family-validator'
+
+
+# TODO: PR: Add Encryption
+# TODO: PR: lock Table
+# TODO: PR: AMI Search vs Fixed AMI.
+# TODO: PR: Validator Client Name
